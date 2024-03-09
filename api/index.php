7adaa24ff39d20ae260907daaa0aaf725c3c090b
KeamKRUB <pawatchijaroen2004@gmail.com>
2024-03-09 19:02:07 +0700

    fix

--- api/index.php
+++ api/index.php
@@ -1,12 +1,5 @@
 <?php
 require_once("_system/_config.php");
-// $connect = new mysqli('https://databases.000webhost.com/', 'id21970520_root', '5rXf>_W0aat4Nfv|', 'id21970520_storyworld');
-
-// Check if the connection is successful
-if ($connect->connect_errno) {
-    die("Failed to connect to MySQL: " . $connect->connect_error);
-}
-else{
+$connect = new mysqli('localhost', 'id21970520_root', '5rXf>_W0aat4Nfv|', 'id21970520_storyworld');
     echo "hello";
-}
 ?>
