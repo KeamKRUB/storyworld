f5b96747178a79d5ed6ff673e7619ea5bfda19ad
KeamKRUB <pawatchijaroen2004@gmail.com>
2024-03-09 18:40:41 +0700

    fix

--- api/index.php
+++ api/index.php
@@ -55,29 +55,31 @@ if(isset($_POST['submit'])){
     }
     $stmt->close();
 }
-$image = new Imagick('image/output.jpg');
+// $image = new Imagick('image/output.jpg');
 if ($query_draw->num_rows > 0) {
-    while ($drawR = mysqli_fetch_assoc($query_draw)) {
-$vectors = explode(' ', $drawR['vector']);
-$draw = new ImagickDraw();
-$draw->setStrokeColor($drawR['color']);
-$draw->setStrokeWidth($drawR['width']);
-$draw->setFillColor('none');
-$draw->pathStart();
-for ($i = 0; $i < count($vectors) - 3; $i += 2) {
-$control_x = ($vectors[$i] + $vectors[$i + 2]) / 2;
-$control_y = ($vectors[$i + 1] + $vectors[$i + 3]) / 2;
-$draw->pathLineToAbsolute($control_x, $control_y);
-}
-$image->drawImage($draw);
-    }
-} else {
-    echo "No rows found in the draw table.";
-}
-$imageFilePath = "image/output.jpg";
-$image->writeImage($imageFilePath);
-$image->clear();
-$image->destroy();
+    while ($drawR = mysqli_fetch_assoc($query_draw)){
+        echo $drawR['id'];
+    } }
+// $vectors = explode(' ', $drawR['vector']);
+// $draw = new ImagickDraw();
+// $draw->setStrokeColor($drawR['color']);
+// $draw->setStrokeWidth($drawR['width']);
+// $draw->setFillColor('none');
+// $draw->pathStart();
+// for ($i = 0; $i < count($vectors) - 3; $i += 2) {
+// $control_x = ($vectors[$i] + $vectors[$i + 2]) / 2;
+// $control_y = ($vectors[$i + 1] + $vectors[$i + 3]) / 2;
+// $draw->pathLineToAbsolute($control_x, $control_y);
+// }
+// $image->drawImage($draw);
+//     }
+// } else {
+//     echo "No rows found in the draw table.";
+// }
+// $imageFilePath = "image/output.jpg";
+// $image->writeImage($imageFilePath);
+// $image->clear();
+// $image->destroy();
 
 $connect->close();
 ?>
